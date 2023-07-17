b64330f4b782ca498f5065506d58b05ca9682abd
m6gorowski <m6gorowski@protonmail.com>
2023-07-17 10:40:50 +0200

    Walls stop moving when player dies

--- Assets/Scripts/WallsMovementScript.cs
+++ Assets/Scripts/WallsMovementScript.cs
@@ -6,9 +6,11 @@ public class WallsMovementScript : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
     [SerializeField] private float _lifespan;
+    public PlayerMovementScript playerMovementScript { get; private set; }
     public Rigidbody2D wallRigidbody { get; private set; }
     void Start()
     {
+        playerMovementScript = FindObjectOfType<PlayerMovementScript>();
         wallRigidbody = GetComponent<Rigidbody2D>();
         wallRigidbody.velocity = Vector3.left * _movementSpeed;
         StartCoroutine(AliveTime(_lifespan));
@@ -16,7 +18,11 @@ public class WallsMovementScript : MonoBehaviour
 
     void Update()
     {
-        
+        if (!playerMovementScript.isPlayerAlive)
+        {
+            StopAllCoroutines();
+            wallRigidbody.velocity = Vector2.zero;
+        }
     }
     public IEnumerator AliveTime(float lifespan)
     {
