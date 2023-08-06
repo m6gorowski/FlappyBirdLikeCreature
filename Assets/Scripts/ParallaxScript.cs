a24e2da814d63a94414664a09730cd04d9bb84c2
m6gorowski <m6gorowski@protonmail.com>
2023-08-06 17:55:48 +0200

    parallax effect in the background

---
+++ Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+    [SerializeField] private float _scrollSpeed = -2f;
+    private float _singleTextureWidth;
+    void Start()
+    {
+        SetupTexture();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+        CheckReset();
+    }
+
+    void SetupTexture()
+    {
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        _singleTextureWidth = sprite.texture.width / (sprite.pixelsPerUnit * 3);
+    }
+
+    void Scroll()
+    {
+        float delta = (_scrollSpeed * Time.deltaTime);
+        transform.position += new Vector3(delta, 0f, 0f);
+    }
+    void CheckReset()
+    {
+        if ( (Mathf.Abs(transform.position.x) - _singleTextureWidth) > 0)
+        {
+            transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+        }
+    }
+}
