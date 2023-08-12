843b254f3c50ca5fc8ef7756a63ac7da82a1ded5
m6gorowski <m6gorowski@protonmail.com>
2023-08-12 17:28:19 +0200

    Restart without pressing the button on screen
    The player doesn't have to restart the game by clicking on the restart button. After a second from losing the game, they can push the "jump" button to restart.

--- Assets/Scripts/RestartScript.cs
+++ Assets/Scripts/RestartScript.cs
@@ -7,6 +7,7 @@ public class RestartScript : MonoBehaviour
 {
     [SerializeField] private Transform _box;
     [SerializeField] private CanvasGroup _background;
+    private bool _canRestartByInput = false;
 
     private void OnEnable()
     {
@@ -15,10 +16,24 @@ public class RestartScript : MonoBehaviour
 
         _box.localPosition = new Vector2(0, -Screen.height);
         _box.LeanMoveLocalY(0, 0.5f).setEaseOutExpo().delay = 0.1f;
+
+        Invoke("AllowRestartingByInput",1f);
+    }
+    private void Update()
+    {
+        if((Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")) && _canRestartByInput)
+        {
+            RestartGame();
+        }
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void AllowRestartingByInput()
+    {
+        _canRestartByInput = true;
     }
 }
