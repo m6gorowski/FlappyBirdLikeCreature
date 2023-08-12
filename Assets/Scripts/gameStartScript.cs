f50e497ee33d578e7f8a6053de758cc8ee74c564
m6gorowski <m6gorowski@protonmail.com>
2023-08-12 18:03:01 +0200

    Points Text appear on start only

--- Assets/Scripts/gameStartScript.cs
+++ Assets/Scripts/gameStartScript.cs
@@ -10,19 +10,20 @@ public class gameStartScript : MonoBehaviour
     private bool isGameActive = false;
     IEnumerator wallSpawning;
     [SerializeField] private GameObject _tutorialText;
+    [SerializeField] private GameObject _pointsText;
     private void Start()
     {
         wallSpawnScript = GetComponent<WallSpawnScript>();
         player = GameObject.Find("Player");
         wallSpawning = wallSpawnScript.SpawnWalls(wallSpawnScript._spawnTime);
     }
-    // Update is called once per frame
     void Update()
     {
         if (!isGameActive && (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space")))
-        {            
+        {   
             StartCoroutine(wallSpawning);
             _tutorialText.SetActive(false);
+            _pointsText.SetActive(true);
             player.GetComponent<PlayerMovementScript>().playerRigidbody.gravityScale = 2;
             player.GetComponent<Animator>().enabled = false;
             isGameActive = true;
